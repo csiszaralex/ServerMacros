7aead4eed673833b00025b4a50f931f8edbca04f
csiszaralex <csiszaralex@gmail.com>
2023-10-22 19:44:15 +0200

    :wrench: Turn cache on

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import appConfig from './config/app.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [appConfig], expandVariables: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [appConfig], expandVariables: true, cache: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
