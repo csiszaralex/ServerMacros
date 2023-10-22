737527c0ced365160b593495d3fa84b0da09722f
csiszaralex <csiszaralex@gmail.com>
2023-10-22 20:14:47 +0200

    :memo: Adds openapi for self documenting endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { AppModule } from './app.module';
 
@@ -9,6 +15,8 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const logger = new Logger('START');
   const port = configService.get('port');
+
+  createSwagger(app);
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
@@ -24,5 +32,24 @@ async function bootstrap() {
   await app.listen(port, () => {
     logger.debug(`---------- Server started on port ${port} ----------`);
   });
+}
+async function createSwagger(app: INestApplication) {
+  const TITLE = 'ServerAPI';
+  const configSW = new DocumentBuilder()
+    .setTitle(TITLE)
+    .setDescription('My server configurato backend')
+    .setVersion(process.env.npm_package_version || '0.1.0')
+    .build();
+  const documentOptions: SwaggerDocumentOptions = {
+    ignoreGlobalPrefix: true,
+    // deepScanRoutes: true,
+  };
+  const customOptions: SwaggerCustomOptions = {
+    customfavIcon: 'https://nestjs.com/img/logo_text.svg',
+    customSiteTitle: TITLE,
+  };
+
+  const document = SwaggerModule.createDocument(app, configSW, documentOptions);
+  SwaggerModule.setup('api', app, document, customOptions);
 }
 bootstrap();
