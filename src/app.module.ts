8f91e3e4d45fae40aca66b60df11587ac68fdfd3
csiszaralex <csiszaralex@gmail.com>
2023-10-22 20:06:27 +0200

    :card_file_box: Adds prisma and config it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,38 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import appConfig from './config/app.config';
+import { PrismaModule, loggingMiddleware, QueryInfo } from 'nestjs-prisma';
+import { AppConfig } from './config/app.config.interface';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [appConfig], expandVariables: true, cache: true }),
+    PrismaModule.forRootAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<AppConfig>) => ({
+        prismaOptions: {
+          log: ['warn', 'error'],
+          errorFormat:
+            configService.get<string>('node_env') === 'development' ? 'pretty' : 'minimal',
+        },
+        prismaServiceOptions: {
+          explicitConnect: true,
+          middlewares: [
+            loggingMiddleware({
+              logger: new Logger('Prisma'),
+              logLevel: 'log',
+              logMessage(query: QueryInfo) {
+                return `${query.model}.${query.action}(${query.executionTime}ms)`;
+              },
+            }),
+          ],
+        },
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,16 @@ async function bootstrap() {
   const logger = new Logger('START');
   const port = configService.get('port');
 
-
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    }),
+  );
 
   await app.listen(port, () => {
     logger.debug(`---------- Server started on port ${port} ----------`);
