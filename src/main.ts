1af6a035dcb8ae2823fff5016a95212320328bad
csiszaralex <csiszaralex@gmail.com>
2023-10-22 19:41:24 +0200

    :wrench: Adds configModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const logger = new Logger('START');
+  const port = configService.get('port');
+
+
+
+  await app.listen(port, () => {
+    logger.debug(`---------- Server started on port ${port} ----------`);
+  });
 }
 bootstrap();
